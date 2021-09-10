3b00b9682b00156f9b6d7b36be99c4a9e2ffc133
wangkui0508 <wangkui0508@hotmail.com>
2021-09-10 11:09:43 +0800

    And packPrice and unpackPrice

--- solidity/test/price.js
+++ solidity/test/price.js
@@ -1,44 +1,117 @@
-// alpha = 1.0069555500567189 = 2**0.01;   alpha**100 = 2
-// 0x40000/0.0069555500567189 = 37688464
-// for i in range(10): print((1<<24)*(alpha**i))
-const BASE = 37688464n;
-const X = (16777216n<<(0n*25n))| //alpha*0
-          (16893910n<<(1n*25n))| //alpha*1
-          (17011417n<<(2n*25n))| //alpha*2
-          (17129740n<<(3n*25n))| //alpha*3
-          (17248887n<<(4n*25n))| //alpha*4
-          (17368863n<<(5n*25n))| //alpha*5
-          (17489673n<<(6n*25n))| //alpha*6
-          (17611323n<<(7n*25n))| //alpha*7
-          (17733819n<<(8n*25n))| //alpha*8
-          (17857168n<<(9n*25n)); //alpha*9
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
 
-// for i in range(10): print((1<<24)*(alpha**(i*10)))
-const Y = (16777216n<<(0n*25n))| //alpha*0
-          (17981375n<<(1n*25n))| //alpha*10
-          (19271960n<<(2n*25n))| //alpha*20
-          (20655176n<<(3n*25n))| //alpha*30
-          (22137669n<<(4n*25n))| //alpha*40
-          (23726566n<<(5n*25n))| //alpha*50
-          (25429504n<<(6n*25n))| //alpha*60
-          (27254668n<<(7n*25n))| //alpha*70
-          (29210830n<<(8n*25n))| //alpha*80
-          (31307392n<<(9n*25n)); //alpha*90
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
 
-const MASK25 = (1n<<26n)-1n;
-const MASK64 = (1n<<65n)-1n;
+        if (sortedArray[middle] === key) {
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
 
-// return a number in the range [2.8147497671065605e-12, 2962330055567.072]
-// maximum returned value: 17857168*31307392*(2**79)=fe3b4ed7d40000000000000000000000 < 2**128
-module.exports.unpackPrice = function(packedPrice) {
-    const priceTick = (packedPrice >> 19n) & 0x1FFFn; // 13 bits
-    const adjust = packedPrice & 0x7FFFFn; // 19 bits
-    const tail = priceTick % 100n;
-    const beforeAdjust = ((Y>>((tail/10n)*25n))&MASK25) * ((X>>(tail%10n)*25n)&MASK25);
-    const priceBase = (beforeAdjust * (BASE+adjust) / BASE) & 0xFFFFFFFFFFFFFFFFn;
-    return [priceBase, priceTick];
+    return start;
 }
 
-module.exports.packPrice = function(price) {
-    // TODO
+function tickToPrice(tick) {
+  X = [16777216, 16893910, 17011417, 17129740, 17248887, 17368863, 17489673, 17611323, 17733819, 17857168]
+  Y = [16777216, 17981375, 19271960, 20655176, 22137669, 23726566, 25429504, 27254668, 29210830, 31307392]
+  const shift = tick/100
+  const tail = tick%100
+  var price = Y[tail/10] * X[tail%10]
+  return price * Math.pow(2, shift) / Math.pow(10,26)
 }
+
+const BASE = 37688464.0
+const ALPHA = 1.0069555500567189
+
+function unpackPrice(packedPrice) {
+  var tick = (packedPrice>>19)&0x1FFF // 13 bits
+  const shift = Math.floor(tick/100)
+  var adjust = 1.0 * (packedPrice & 0x7FFFF) // 19 bits
+  var X = [16777216, 16893910, 17011417, 17129740, 17248887, 17368863, 17489673, 17611323, 17733819, 17857168]
+  var Y = [16777216, 17981375, 19271960, 20655176, 22137669, 23726566, 25429504, 27254668, 29210830, 31307392]
+  const tail = (tick%100)|0x0
+  var beforeAdjust = 1.0 * (Y[Math.floor(tail/10)] * X[tail%10])
+  const priceBase = beforeAdjust * (BASE + adjust) / BASE
+  const price = priceBase * Math.pow(2.0, shift) / Math.pow(10.0, 26)
+  //console.log("shift ", shift, "tick ", tick, "beforeAdjust", beforeAdjust, "priceBase", priceBase, "price", price)
+  return price
+}
+
+function getPriceList() {
+  var res = []
+  for (var i=0; i<7901; i+=10) {
+  	res.push(unpackPrice(i<<19))
+  }
+  return res
+}
+
+var PriceList = getPriceList()
+
+function printPriceList() {
+  for (var i=0; i<PriceList.length; i++) {
+  	console.log(PriceList[i])
+  }
+}
+
+function estimateTick(price) {
+  var idx = binarySearch(PriceList, price)
+  if(idx >= PriceList.length) {
+    idx = PriceList.length-1
+  }
+  if(PriceList[idx]>price) {
+    idx--;
+  }
+  //console.log("idx ", idx, "in-table ", PriceList[idx], "price", price)
+  return idx;
+}
+
+function packPrice(price) {
+  const idx = estimateTick(price)
+  var tick = idx*10;
+  var startPrice = PriceList[idx]
+  while(true) {
+    const newPrice = startPrice*ALPHA
+    if(newPrice > price) {
+      break
+    }
+    startPrice = newPrice
+    tick++
+  }
+  var delta = (price - startPrice) / price
+  var adjust = delta*BASE
+  maxAdjust = Math.pow(2.0, 19) - 1.0
+  if(adjust > maxAdjust) {
+    adjust = maxAdjust
+  }
+  //console.log("tick ", tick, "startPrice", startPrice, "price", price, "delta", delta, "adjust", adjust)
+  var hi = (tick&0x1FFF)<<19
+  var lo = adjust&0x7FFFF
+  return (hi | lo)
+}
+
+function testPrice(price) {
+  console.log("------------")
+  console.log("origin ", price)
+  console.log("encdec ", unpackPrice(packPrice(price)))
+}
+
+function test() {
+  testPrice(2.8147497671065605e-12)
+  testPrice(1.2933193792219687e-11)
+  testPrice(1.2943193792219687e-11)
+  testPrice(0.12)
+  testPrice(1.2)
+  testPrice(12)
+  testPrice(22600775.023599107)
+  testPrice(1587473070415)
+  testPrice(25961484.84836226)
+	
+}
+
+test()
