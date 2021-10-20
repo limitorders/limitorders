5207be2c1079340110818b6e6e6f166811501c88
wangkui0508 <wangkui0508@hotmail.com>
2021-10-20 15:11:07 +0800

    Fix image's URL

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav" style="font-size: 30px">
   <p style="text-align: center"><a href="javascript:" @click.prevent.stop="home">
-  <img style="height: 60px" src="favicon.svg"><img width=260 src="gridex.jpg"></a></p>
+  <img style="height: 60px" src="/favicon.svg"><img width=260 src="/gridex.jpg"></a></p>
     <router-link tag="li" to="/markets">Markets</router-link><span style="color: #EEEEEE"> | </span>
     <router-link tag="li" to="/grid">Grid Trading</router-link><span style="color: #EEEEEE"> | </span>
     <router-link tag="li" to="/exchange">Exchange</router-link><span style="color: #EEEEEE"> | </span>
